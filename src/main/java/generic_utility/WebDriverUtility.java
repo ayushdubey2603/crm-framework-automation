b1920ba4cb10d4b64e4bba57ba1d2881e64822bb
DELL <DELL@192.168.1.3>
2025-09-02 17:33:51 +0530

    commit

---
+++ src/main/java/generic_utility/WebDriverUtility.java
@@ -0,0 +1,14 @@
+package generic_utility;
+
+public class WebDriverUtility {
+
+	WebDriverUtility(){
+		
+	}
+	
+	public static void main(String[] args) {
+		WebDriverUtility wdUtil = new WebDriverUtility();
+
+	}
+
+}
